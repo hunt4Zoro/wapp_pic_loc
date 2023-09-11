51069b5ed0634b1b1fca11c68c7ea92e7fef1c89
Adil Ahamed <113287188+hunt4Zoro@users.noreply.github.com>
2023-09-11 12:47:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+  const captureBtn = document.getElementById('capture-btn');
+  const previewContainer = document.getElementById('preview-container');
+  const locationDisplay = document.getElementById('location');
+
+  captureBtn.addEventListener('click', () => {
+    captureAndSavePhoto();
+    getLocation();
+  });
+
+  function captureAndSavePhoto() {
+    const constraints = { video: { facingMode: 'environment' } };
+
+    navigator.mediaDevices.getUserMedia(constraints)
+      .then(function(mediaStream) {
+        const video = document.createElement('video');
+        video.srcObject = mediaStream;
+        video.autoplay = true;
+        previewContainer.innerHTML = '';
+        previewContainer.appendChild(video);
+
+        const canvas = document.createElement('canvas');
+        const context = canvas.getContext('2d');
+        context.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
+        const imageDataUrl = canvas.toDataURL('image/jpeg');
+        savePhotoLocally(imageDataUrl);
+      })
+      .catch(function(err) {
+        console.error('Error accessing camera:', err);
+      });
+  }
+
+  function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+      locationDisplay.textContent = 'Geolocation is not supported by this browser.';
+    }
+  }
+
+  function showPosition(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    locationDisplay.textContent = `Latitude: ${latitude.toFixed(6)}, Longitude: ${longitude.toFixed(6)}`;
+  }
+
+  function savePhotoLocally(imageDataUrl) {
+    const link = document.createElement('a');
+    link.href = imageDataUrl;
+    link.download = 'photo.jpg';
+    link.click();
+  }
+});
